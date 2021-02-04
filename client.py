554ec33368d18ca0498ca09b2d430f89df7a48fd
Kelsey Williams <kelseywilliams1200@gmail.com>
2021-02-04 13:57:51 -0800

    Changed inaccuracy in a comment

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-#Hello future me.  In the event that you do decide to run this program, most of it should work, however,
+# A note to myself.  In the event that you do decide to run this program, most of it should work, however,
 # the google api code may need some reworking as google interface code usually does.  In the event that
 # the code runs smoothly and you have the proper credentials stashed in this directory and have the google
 # sdk installed and operational or whatever the hell google wants, the bash script "set_tts_var" has been added
@@ -18,9 +18,10 @@ client = texttospeech.TextToSpeechClient()
 while True:
     req = requests.post("https://kelseywilliams.net/TTS-Server/api.php", data = {"password":password.PASSWORD,"filter":"unread"})
 
-    # It's important to note that if the json response holds no keys, it cannot be converted to a dictionary as python won't create
-    # dict's without keys.  Therefore the response is of type str.  This took a minute to figure out and it's why the len of the
-    # response is unreliable for checking whether or not data is present in the response -- That requires a quick isinstance() call.
+    # It's important to note that if the json response holds no keys, the requests library will 
+    # generate a str instead of a dict.  This took a minute to figure out and it's why the len of the
+    # response is unreliable for checking whether or not data is present (len of '{}' returns 2
+    # but len of {} returns 0).  This is fixed though with a quick isinstance() call.
     posts = req.json()
     
     if isinstance(posts, dict):
